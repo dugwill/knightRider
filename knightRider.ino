5a0fe58c941bcb488c8c150dd1d5836c6db82d82
dwill0849e_comcast <douglas_will@cable.comcast.com>
2024-02-24 08:33:49 -0700

    Initial commit

---
+++ knightRider.ino
@@ -0,0 +1,27 @@
+#include <FastLED.h>
+#define NUM_LEDS 6
+#define DECAY_VALUE 200  // higher is faster
+#define DELAY 140  //higher is slower
+
+
+CRGB leds[NUM_LEDS];
+void setup() { FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS); }
+void loop() {
+        for(int dot = 0; dot < NUM_LEDS; dot++) {
+            leds[dot] = CRGB::Red;
+            FastLED.show();
+            decay(leds);
+            delay(DELAY);
+        }
+        for(int dot = NUM_LEDS-1; dot >=0; dot--) {
+            leds[dot] = CRGB::Red;
+            FastLED.show();
+            decay(leds);
+            delay(DELAY);
+        }
+    }
+
+
+void decay(CRGB leds[NUM_LEDS]){
+    fadeToBlackBy(leds,NUM_LEDS,DECAY_VALUE);
+}
